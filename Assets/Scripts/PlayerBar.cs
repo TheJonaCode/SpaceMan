811524c846b70242d3a728d94131b804afab8460
TheJonaCode <mcjhona069@gmail.com>
2022-10-24 13:40:30 -0500

    Programando las barras de vida y mana

---
+++ Assets/Scripts/PlayerBar.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BarType{
+    healthBar,
+    manaBar
+}
+
+public class PlayerBar : MonoBehaviour
+{
+    private Slider slider;
+    public BarType type;
+
+    void Start(){
+        slider = GetComponent<Slider>();
+        switch(type){
+            case BarType.healthBar:
+                slider.maxValue = PlayerControler.MAX_HEALTH;
+            break;
+
+            case BarType.manaBar:
+                slider.maxValue = PlayerControler.MAX_MANA;
+            break;
+        }
+    }
+
+    void Update(){
+        switch(type){
+            case BarType.healthBar:
+                slider.value = GameObject.Find("Player").
+                    GetComponent<PlayerControler>().GetHealth();
+            break;
+
+            case BarType.manaBar:
+                slider.value = GameObject.Find("Player").
+                    GetComponent<PlayerControler>().GetMana();
+            break;
+        }
+    }
+}
