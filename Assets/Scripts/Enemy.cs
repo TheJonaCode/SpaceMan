ba6bce55673de136c3bb8d623b48e14f71f35c38
TheJonaCode <mcjhona069@gmail.com>
2022-10-30 17:18:39 -0600

    Enemigos moviles - preparando giro

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -22,7 +22,24 @@ public class Enemy : MonoBehaviour
         this.transform.position = startPosition;
     }
 
-    void Update(){
-        
+    private void FixedUpdate(){
+        float currentRunningSpeed = runningSpeed;
+
+        if(facingRight){
+            //Mirando hacia la derecha
+            currentRunningSpeed = runningSpeed;
+            this.transform.eulerAngles = new Vector3(0, 180, 0);
+        }else{
+            //Mirando hacia la izquierda
+            //Velocidad en negativo
+            currentRunningSpeed = -runningSpeed;
+            this.transform.eulerAngles = Vector3.zero;
+        }
+
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame){
+            rigidBody.velocity = new Vector2(currentRunningSpeed, rigidBody.velocity.y);
+        }
+
     }
+
 }
